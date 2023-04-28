e048562333620767c1c3cc92819d67e182a292b2
김지호 <crsn1111@gmail.com>
2023-04-28 23:59:29 +0900

    /feat 2023-04-29 charley

---
+++ src/Charley/23-04-29/약수의 개수와 덧셈.js
@@ -0,0 +1,15 @@
+function solution(left, right) {
+  let answer = 0;
+  for (let i = left; i <= right; i++) {
+    let p = 1;
+    for (let j = 2; j <= i; j++) {
+      if (i % j === 0) p++
+    }
+    (p % 2 === 0) ? answer += i : answer -= i
+    // 제곱근이 정수면 약수의 개수는 홀수이다
+    //(Number.isInteger(Math.sqrt(i))) ? answer -= i : answer += i;
+  }
+  return answer
+}
+
+console.log(solution(13, 17))
