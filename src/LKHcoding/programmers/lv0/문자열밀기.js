3129fd26b2a74183a73566f07336a57ddff2eda9
lkhcoding <tha920@naver.com>
2023-05-09 01:39:48 +0900

    문자열 밀기 다른 해법

--- src/LKHcoding/programmers/lv0/문자열밀기.js
+++ src/LKHcoding/programmers/lv0/문자열밀기.js
@@ -1,16 +1,22 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/120921
 function solution(A, B) {
-  let answer = 0;
+  /** B를 두번 더한다음에 A가 몇번째에 있는지 찾으면 됨 발상의 전환이 필요한 문제
+   * 예를들어 A: hello, B: ohell 이라면
+   * ohellohell <- 여기서 hello 를 찾으면 됨
+   */
+  return (B + B).indexOf(A);
 
-  const arrA = A.split('');
-
-  for (let i = 0; i <= A.length - 1; ++i) {
-    if (arrA.join('') === B) {
-      return i;
-    }
-    arrA.unshift(arrA.pop());
-    answer++;
-  }
-
-  return answer ? -1 : answer;
+  // let answer = 0;
+  //
+  // const arrA = A.split('');
+  //
+  // for (let i = 0; i <= A.length - 1; ++i) {
+  //   if (arrA.join('') === B) {
+  //     return i;
+  //   }
+  //   arrA.unshift(arrA.pop());
+  //   answer++;
+  // }
+  //
+  // return answer ? -1 : answer;
 }
