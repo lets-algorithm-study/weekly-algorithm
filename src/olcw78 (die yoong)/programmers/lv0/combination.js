982e5e9356284163ab7fadb58c3465eaf09e107d
yoonsang lee <highp0912@pm.me>
2023-05-10 13:48:06 +0900

    이름 수정

--- src/olcw78 (die yoong)/programmers/lv0/combination.js
+++ src/olcw78 (die yoong)/programmers/lv0/combination.js
@@ -1,10 +1,12 @@
-function fibonacci(n) {
+function factorial(n) {
   if (n === 1 || n === 0) return 1;
-  return n * fibonacci(n - 1);
+  return n * factorial(n - 1);
 }
 
+// nCm -> n개 중 m 개를 순서 상관없이 뽑는 경우의 수
+// n! / ((n-m)! * m!)
 function solution(balls, share) {
-  return fibonacci(balls) / (fibonacci(balls - share) * fibonacci(share));
+  return factorial(balls) / (factorial(balls - share) * factorial(share));
 }
 
 console.log(solution(30, 0));
