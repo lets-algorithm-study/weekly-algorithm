2ed26bdfb2ee563a25ecadbb4df1f27adb9e0429
lkhcoding <tha920@naver.com>
2023-05-11 23:10:08 +0900

    직사각형 넓이 구하기 풀이

---
+++ src/LKHcoding/programmers/lv0/직사각형넓이구하기.js
@@ -0,0 +1,26 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120860
+function solution(dots) {
+  let minX = Infinity;
+  let maxX = -Infinity;
+
+  let minY = Infinity;
+  let maxY = -Infinity;
+
+  dots.forEach(item => {
+    if (item[0] <= minX) {
+      minX = item[0];
+    }
+    if (item[0] >= maxX) {
+      maxX = item[0];
+    }
+
+    if (item[1] <= minY) {
+      minY = item[1];
+    }
+    if (item[1] >= maxY) {
+      maxY = item[1];
+    }
+  });
+
+  return (maxX - minX) * (maxY - minY);
+}
