33088add9f7b63811e338a822fffcc115dea28b7
김지호 <crsn1111@gmail.com>
2023-05-13 11:50:48 +0900

    23-05-13 charley

--- src/Charley/23-05-13/k의 개수.js
+++ src/Charley/23-05-13/k의 개수.js
@@ -2,11 +2,12 @@ https://school.programmers.co.kr/learn/courses/30/lessons/120887
 
 const solution = (i, j, k) => {
   let result = 0;
+  let k = String(k);
   for (i; i <= j; i++) {
-    if (String(i).includes(String(k))) {
+    if (String(i).includes(k)) {
       // 11, 22, 333 처럼 하나의 값에 필요한 k의 값이 중복으로 보유한 경우
       for (let j = 0; j < i.length; j++) {
-        if (double[j] === String(k)) {
+        if (double[j] === k) {
           result++
         }
       }
