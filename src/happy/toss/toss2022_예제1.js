b0e68d1515bbbfdce194d3fe8bd4460cb9948e95
yunyami <yunyami0605@gmail.com>
2023-07-08 10:44:00 +0900

    feat: 토스 문제3 추가

--- src/happy/toss/toss2022_예제1.js
+++ src/happy/toss/toss2022_예제1.js
@@ -1,3 +1,34 @@
+/**
+  문제명 : No More Undefined
+  김토스는 토스에서 외부 SDK를 연동하고 있습니다.
+
+  연동하던 중 아래와 같이 중간에 어떤 값이라도 undefined 일 수 있는 객체를 다뤄야 한다는 것을 알게 되었습니다.
+
+
+  const object1 = {
+    repository: undefined,
+  };
+
+  const object2 = {
+    repository: {
+      readme: undefined,
+    },
+  };
+
+  const object3 = {
+    repository: {
+      readme: {
+        extension: 'md',
+        content: '금융을 쉽고 간편하게',
+      }
+    }
+  };
+
+  위와 같은 객체를 쉽게 다룰 수 있도록 유틸리티 함수를 만들려고 합니다.
+
+  깊게 undefined 일 수 있는 객체의 값을 안전하게 가져올 수 있도록 safelyGet 함수를 구현해 주세요.
+ */
+
 
 function safelyGet(obj, str){
     let tmp = str.split(".");
