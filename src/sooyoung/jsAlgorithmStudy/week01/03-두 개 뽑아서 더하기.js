60e2b3753c045dd709240be762ca9199a029f0cc
sooyoung <sooyoung159@naver.com>
2024-12-07 10:19:05 +0900

    2주차 알고리즘

---
+++ src/sooyoung/jsAlgorithmStudy/week01/03-두 개 뽑아서 더하기.js
@@ -0,0 +1,30 @@
+function solution(numbers) {
+  let answer = [];
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      let sum = numbers[i] + numbers[j];
+      if (!answer.includes(sum)) answer.push(numbers[i] + numbers[j]);
+    }
+  }
+  return Array.from(answer).sort((a, b) => a - b);
+}
+
+// 시간복잡도: O(n^3)
+
+// set이 나은건가 배열에서 바로 찾는게 나은건가
+
+// function solution(numbers) {
+//   const temp = []
+//
+//   for (let i = 0; i < numbers.length; i++) {
+//     for (let j = i + 1; j < numbers.length; j++) {
+//       temp.push(numbers[i] + numbers[j])
+//     }
+//   }
+//
+//   const answer = [...new Set(temp)]
+//
+//   return answer.sort((a, b) => a - b)
+// }
+
+// 시간복잡도: O(n^2 log n)
