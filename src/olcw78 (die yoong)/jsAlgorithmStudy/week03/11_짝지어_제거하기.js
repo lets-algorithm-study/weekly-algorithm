29cdad0053afebd07ee9aa811b3a9114b54ffa5e
yoonsang lee <highp0912@pm.me>
2024-12-07 19:24:57 +0900

    doc: 코드 변경 이유 추가

--- src/olcw78 (die yoong)/jsAlgorithmStudy/week03/11_짝지어_제거하기.js
+++ src/olcw78 (die yoong)/jsAlgorithmStudy/week03/11_짝지어_제거하기.js
@@ -6,6 +6,35 @@
  * @param s {string}
  * @return {1 | 0}
  */
+function solution1(s) {
+    const stack = [s.at(0)]
+
+    for (const ch of s.slice(1)) {
+        if (stack[stack.length - 1] === ch) {
+            stack.pop()
+            continue
+        }
+
+        stack.push(ch)
+    }
+
+    return stack.length === 0 ? 1 : 0
+}
+
+const r3 = solution("baabaa")
+console.log(r3)
+
+const r4 = solution("cdcd")
+console.log(r4)
+
+/**
+ *
+ * 스택이 아닌 직접 문자열을 소거하는 방식.
+ * 2중 루프로 TC O(N^2) 1M ^2 이므로 시간 초과.
+ *
+ * @param s {string}
+ * @return {1 | 0}
+ */
 // function solution(s) {
 //     let cur = null
 //     let prv = null
@@ -49,33 +78,4 @@ console.log(r1)
 
 const r2 = solution("cdcd")
 console.log(r2)
-
-/**
- *
- * 제약조건
- * s 길이: 1M 이하의 자연수
- *
- * @param s {string}
- * @return {1 | 0}
- */
-function solution1(s) {
-    const stack = [s.at(0)]
-
-    for (const ch of s.slice(1)) {
-        if (stack[stack.length - 1] === ch) {
-            stack.pop()
-            continue
-        }
-
-        stack.push(ch)
-    }
-
-    return stack.length === 0 ? 1 : 0
-}
-
-const r3 = solution("baabaa")
-console.log(r3)
-
-const r4 = solution("cdcd")
-console.log(r4)
 
