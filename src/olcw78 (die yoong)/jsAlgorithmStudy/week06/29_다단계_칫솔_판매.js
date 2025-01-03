1c38a9b92c9843baf5975cee475b0df743fee0bd
yoonsang lee <highp0912@pm.me>
2025-01-04 03:20:16 +0900

    add: 듀뎍

--- src/olcw78 (die yoong)/jsAlgorithmStudy/week06/29_다단계_칫솔_판매.js
+++ src/olcw78 (die yoong)/jsAlgorithmStudy/week06/29_다단계_칫솔_판매.js
@@ -9,19 +9,26 @@ function solution(enroll, referral, seller, amount) {
     const ret = Array.from({length: referral.length}).fill(0);
     const PRICE = 100;
     for (let i = 0; i < seller.length; i++) {
+        // 첫 사람 인덱스, 이름
         let j = enroll.findIndex(x => x === seller[i]);
         let next = enroll[j];
+        // 첫 가격 90%만 가져감
         let start = amount[i] * PRICE * 0.9
         ret[j] += start;
         // console.log(`#${i} ${next}, ${sum}>`);
 
+        // - 가 나오면 반복 종료
         while (next && next !== "-") {
+            // 이어진 사람 가져오기
             next = referral[j];
+            // 다음 이어질 사람의 인덱스 찾기
             j = enroll.findIndex(x => x === next);
+            // -> 못찾으면 center(민수) 선택된 것
             if (j === -1) break;
 
+            // 10% 만 연결된 사람들에게 줌
             const rest = Math.floor(start * 0.1);
-            start = rest;
+            start = rest; // 10%의 다음가격에서 다시 계산을 함.
             ret[j] += rest;
             // console.log(`#${i} ${next}, ${rest}`);
         }
