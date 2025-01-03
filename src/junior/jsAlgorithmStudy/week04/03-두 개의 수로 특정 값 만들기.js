dad412e0f34ff9142e9ce69fd4f6e668f55f2f6d
dodoheeee <hiyoun96@naver.com>
2025-01-03 23:19:21 +0900

    feat : add week4 sudocode

--- src/junior/jsAlgorithmStudy/week04/03-두 개의 수로 특정 값 만들기.js
+++ src/junior/jsAlgorithmStudy/week04/03-두 개의 수로 특정 값 만들기.js
@@ -1,3 +1,16 @@
+// arr에 합이 target이 되는 두 개의 수가 있는지 확인하라.
+// 1. arr 정수 배열, K 정수
+// 2. 크기가 k+1인 배열을 만들고 0으로 초기화
+// 3. 정수 배열을 돌면서, 현재 값이 k 이하면, hashtable에 1로 표시
+// 4. hashtable을 반환
+
+// 1. arr 정수 배열, target 목표 합
+// 2. countSort 함수를 통해 hashtable을 만든다.
+// 3. 정수 배열을 돌면서, 현재 숫자(num)에 대해 complement = target - num을 계산한다.
+// 4. 민약 complement 이 num이랑 같지 않다.(중복제거)
+// 5. complement가 0 이상이고, target 이하이고, hashtable에 complement가 1로 표시되어 있다면, true를 반환한다.
+// 6. 다 만족하지 않으면 false
+
 function countSort(arr, k) {
   let hashtable = new Array(k + 1).fill(0);
 
--- src/junior/jsAlgorithmStudy/week04/05-완주하지 못한 선수.js
+++ src/junior/jsAlgorithmStudy/week04/05-완주하지 못한 선수.js
@@ -1,3 +1,13 @@
+// 완주하지 못한 선수 이름 구하기
+
+// participant: 참가자들의 이름 배열
+// completion: 완주한 사람들의 이름 배열
+
+// 1. 각 참가자의 이름과 참가 횟수를 저장하기 위한 해시 테이블 역할을 하는 배열을 만든다.
+// 2. 참가자들의 이름을 돌면서, obj에 현재 이름(p)가 있으면, obj[p]에 1을 더하고, 없으면 1로 초기화한다.
+// 3. 완주자들의 이름을 돌면서, obj[c]에 1을 뺀다.
+// 4. obj를 돌면서, 0보다 큰 값이 있으면 해당 이름을 반환한다.
+
 function solution(participant, completion) {
   const obj = [];
 
@@ -20,4 +30,4 @@ function solution(participant, completion) {
   }
 }
 
-console.log(solution(['leo', 'kiki', 'eden'], ['eden', 'kiki']));
+console.log(solution(['leo', 'kiki', 'eden'], ['eden', 'kiki'])); // leo
